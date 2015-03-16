ab3e9969093daf03f960d94e4a053c627e9ff04c
Alban Montaigu <alban.montaigu@gmail.com>
2015-03-16 12:46:23 +0100

    Fix to enable docker cmd without 404
    By default docker 1.5.0 gives 404 errors for each cmd. This will fix it
    until docker 1.5.1

--- etc/boot2docker/custom-bootlocal.sh
+++ etc/boot2docker/custom-bootlocal.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+#
 # Waiting for /vagrant because it may be not mounted
+#
 for i in `seq 1 5`; do 
     echo "== Checking existence of /vagrant !"
     if [ -d /vagrant ]; then
@@ -13,6 +15,20 @@ for i in `seq 1 5`; do
 done
 
 
+#
 # ADD Personal alias to docker user's profile
+#
 cat /var/lib/boot2docker/custom-profile >> /home/docker/.ashrc
 
+#
+# In docker 1.5.0, .sock required in NO_PROXY to avoid 404 on commands.
+# Will be fixed in docker 1.5.1.
+#
+echo "== Checking existence .sock in NO_PROXY !"
+if (env | grep -q .sock); then
+	echo "== .sock already in NO_PROXY !"
+else
+	NO_PROXY="${NO_PROXY},.sock"
+	no_proxy="${no_proxy},.sock"
+	echo "== .sock added in NO_PROXY !"
+fi
