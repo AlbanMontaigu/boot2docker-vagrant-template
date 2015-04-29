65dfba5a1d4da5d8f723863686782fd2413abab2
Alban Montaigu <alban.montaigu@gmail.com>
2015-04-29 10:57:42 +0200

    Do not init db if already present

--- docker/sample-db/init/sample-db-init.sh
+++ docker/sample-db/init/sample-db-init.sh
@@ -4,16 +4,19 @@
 # Waiting for DB
 # -----------------------------------------
 
+# Prepare user & pass in default args
+MYSQL_ARGS="-h$MYSQL_HOST -P$MYSQL_PORT -uroot -p$MYSQL_ROOT_PASSWORD"
+
 # Show env for log
 echo "== Showing env"
 env
 
-db_online="KO"
+# Checking DB connection
 echo "== Checking DB connection !"
 for i in `seq 1 10`; do 
-    db_online=$(mysql -h$MYSQL_HOST -P$MYSQL_PORT -uroot -p$MYSQL_ROOT_PASSWORD -N -s -e 'SELECT "OK";' || echo "KO")
-    if [ $db_online = "OK" ]
-	then
+    db_online=$(mysql $MYSQL_ARGS -N -s -e 'SELECT "OK";' || echo "KO")
+    if [ "$db_online" = "OK" ]
+    then
         echo "== DB found !"
         break
     else
@@ -29,14 +32,23 @@ then
     exit 1
 fi
 
+
 # -----------------------------------------
 # DB initialisation
 # -----------------------------------------
 
+# If already init then exit
+echo "== Checking if DB is already init"
+db_check=$(mysql $MYSQL_ARGS -N -s -e 'SHOW DATABASES LIKE "my_database";' || echo "KO")
+if [ "$db_check" = "my_database" ]
+then
+    echo "== DB already created, nothing to do"
+    exit
+else
+    echo "== No DB created let's do it !"
+fi
+
 # DB scripts execution
-echo "== Executing 1_ump-db-init-droits.sql"
-mysql -h$MYSQL_HOST -P$MYSQL_PORT -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/ump-db/init/1_ump-db-init-droits.sql
-echo "== Executing 2_ump-db-init-portail.sql"
-mysql -h$MYSQL_HOST -P$MYSQL_PORT -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/ump-db/init/2_ump-db-init-portail.sql
-echo "== Executing 3_ump-db-init-user.sql"
-mysql -h$MYSQL_HOST -P$MYSQL_PORT -uroot -p$MYSQL_ROOT_PASSWORD < /tmp/ump-db/init/3_ump-db-init-user.sql
+echo "== Executing 1_sample-db-init.sql"
+mysql $MYSQL_ARGS < /tmp/ump-db/init/1_sample-db-init.sql
+
