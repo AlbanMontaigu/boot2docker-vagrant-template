5363650bd652c64fdb77c71fcf5d8cdd354ab875
Alban Montaigu <alban.montaigu@gmail.com>
2016-02-07 17:04:57 +0100

    Bugfix

--- boot2docker/bootlocal.sh
+++ boot2docker/bootlocal.sh
@@ -5,13 +5,13 @@
 # -------------------------------------------------
 BOOT2DOCKER_CONFIG_DIR="/var/lib/boot2docker/config"
 BOOT2DOCKER_CONFIG_BOOTLOCAL="$BOOT2DOCKER_CONFIG_DIR/bootlocal.sh"
-LOGFILE="$BOOT2DOCKER_CONFIG_DIR/bootlocal.log"
+LOGFILE="/var/log/vagrant-bootlocal.log"
 
 # -------------------------------------------------
 # Execute bootlocal.sh if present to enable
 # live customization
 # -------------------------------------------------
-if [ -f $BOOT2DOCKER_CONFIG_DIR ]; then
+if [ -f $BOOT2DOCKER_CONFIG_BOOTLOCAL ]; then
    echo "== BOOTLOCAL execution..."
    $BOOT2DOCKER_CONFIG_BOOTLOCAL > $LOGFILE
 else
--- boot2docker/provision.sh
+++ boot2docker/provision.sh
@@ -5,7 +5,7 @@
 # -------------------------------------------------
 BOOT2DOCKER_CONFIG_DIR="/var/lib/boot2docker/config"
 BOOT2DOCKER_VAGRANT_CONFIG_REPO="https://github.com/AlbanMontaigu/boot2docker-vagrant-config.git"
-LOGFILE="$BOOT2DOCKER_CONFIG_DIR/provision.log"
+LOGFILE="/var/log/vagrant-provision.log"
 
 # -------------------------------------------------
 # Installing boo2docker configuration files
