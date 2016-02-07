49e546ec1531f8647d1fba4db3bf6411b622e681
Alban Montaigu <alban.montaigu@gmail.com>
2016-02-07 23:11:04 +0100

    Local provisioning enabled

--- boot2docker/provision.sh
+++ boot2docker/provision.sh
@@ -11,9 +11,15 @@ LOGFILE="/var/lib/boot2docker/log/vagrant-provision.log"
 # -------------------------------------------------
 # Installing boo2docker configuration files
 # -------------------------------------------------
-echo "== PROVISIONING files... [$(date)]"
+echo "== PROVISIONING getting files... [$(date)]"
 sudo rm -rvf $BOOT2DOCKER_CONFIG_DIR >> $LOGFILE 2>&1
 sudo git clone $VAGRANT_B2D_CONFIG_REPO $BOOT2DOCKER_CONFIG_DIR >> $LOGFILE 2>&1
 cd $BOOT2DOCKER_CONFIG_DIR
 sudo git checkout $VAGRANT_B2D_CONFIG_VERSION >> $LOGFILE 2>&1
 sudo chmod -R 777 ${BOOT2DOCKER_CONFIG_DIR} >> $LOGFILE 2>&1
+
+# -------------------------------------------------
+# Running local provisionning file
+# -------------------------------------------------
+echo "== PROVISIONING executing local... [$(date)]"
+$BOOT2DOCKER_CONFIG_DIR/provision.sh >> $LOGFILE 2>&1
