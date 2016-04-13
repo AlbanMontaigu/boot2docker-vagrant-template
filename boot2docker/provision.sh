8d711466d8c549a299ff81544112da706e765bb5
Alban Montaigu <alban.montaigu@gmail.com>
2016-04-13 17:02:47 +0200

    Try to be sure that proxy env is loaded

--- boot2docker/provision.sh
+++ boot2docker/provision.sh
@@ -5,9 +5,19 @@
 # -------------------------------------------------
 VAGRANT_B2D_EXTENSION_REPO="$1"
 VAGRANT_B2D_EXTENSION_VERSION="$2"
+BOOT2DOCKER_PROXY_SCRIPT="/etc/profile.d/proxy.sh"
 BOOT2DOCKER_EXTENSION_DIR="/var/lib/boot2docker/extension"
 BOOT2DOCKER_EXTENSION_DIR_TMP="/tmp/extension"
 LOGFILE="/var/lib/boot2docker/log/vagrant-provision.log"
+
+
+# -------------------------------------------------
+# Be sure proxy is loaded if any
+# -------------------------------------------------
+if [ -f $BOOT2DOCKER_PROXY_SCRIPT]; then
+    source $BOOT2DOCKER_PROXY_SCRIPT
+fi
+
 
 # -------------------------------------------------
 # Installing boo2docker configuration files
@@ -22,8 +32,9 @@ cd ..
 sudo mv -fv $BOOT2DOCKER_EXTENSION_DIR_TMP $BOOT2DOCKER_EXTENSION_DIR >> $LOGFILE 2>&1
 sudo chmod -R 777 $BOOT2DOCKER_EXTENSION_DIR >> $LOGFILE 2>&1
 
+
 # -------------------------------------------------
 # Running local provisionning file
 # -------------------------------------------------
 echo "== PROVISIONING running... [$(date)]"
-$BOOT2DOCKER_EXTENSION_DIR/provision.sh >> $LOGFILE 2>&1
+source $BOOT2DOCKER_EXTENSION_DIR/provision.sh >> $LOGFILE 2>&1
