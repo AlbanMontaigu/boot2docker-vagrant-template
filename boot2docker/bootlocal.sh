1a22ca7440b76409533a493895fce6e5c12bcd57
Alban Montaigu <alban.montaigu@gmail.com>
2016-11-02 22:51:28 +0100

    Typo

--- boot2docker/bootlocal.sh
+++ boot2docker/bootlocal.sh
@@ -15,7 +15,7 @@ LOGFILE="/var/lib/boot2docker/log/vagrant-bootlocal.log"
 if [ -f $BOOT2DOCKER_EXTENSION_BOOTLOCAL ]; then
    echo "[INFO][BOOTLOCAL] Starting... ($(date))"  | tee -a $LOGFILE
    $BOOT2DOCKER_EXTENSION_BOOTLOCAL 2>&1 | tee -a $LOGFILE
-   echo "[INFO][BOOTLOCAL] Ended ! ($(date))"  | tee -a $LOGFILE
+   echo "[INFO][BOOTLOCAL] Ended ! ($(date))" | tee -a $LOGFILE
 else
-   echo "[WARN][BOOTLOCAL] Bootlocal not found, try PROVISION before"  | tee -a $LOGFILE
+   echo "[WARN][BOOTLOCAL] Bootlocal not found, try PROVISION before" | tee -a $LOGFILE
 fi
